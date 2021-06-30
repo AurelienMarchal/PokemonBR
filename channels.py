2f299543a74c4107e4bd716bbe6c86b65bdcb775
AurelienMarchal <marchal.aure@gmail.com>
2021-06-30 16:56:11 +0200

    test

--- channels.py
+++ channels.py
@@ -30,7 +30,7 @@ def connect(c, addr):
 @sgn.channel("disconnect")
 def disconnect(c, addr):
     print(sgn.userdata)
-    #print("Client with id :", sgn.callvariable('id', c), "disconnected from the server")
+    print("Client with id :", sgn.callvariable('id', c), "disconnected from the server")
     pass
 
 
